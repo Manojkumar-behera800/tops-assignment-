5e1d92643d79b70231cf3007aaa191a8053a5551
Manoj Behera <mb647004@gmail.com>
2026-04-04 08:50:21 +0530

    initial

---
+++ c lang/relation.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main(){
+	/* int age =20;
+	printf("\n3==5=%d",3==5);
+	printf("\n5> 3=%d",5>3);
+	printf("\n 5<3=%d",5<3);
+	*/
+	int x =10;
+	/*x+=30;//x=x+30
+	printf("\n enterX=%d",x);
+	x*=2;//x=x*2
+	printf("\n enter x=%d",x);
+	x-=10;
+	printf("find value of x:");
+	scanf("%d",&x);
+	printf("\n find x value =%d ",x);
+	*/
+	x/=70;// x=x/20
+	printf("eneter the value of x=%d");
+	
+	
+	
+}
---
+++ c lang/size..c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main(){
+	int age=18,num=-2,a =2 ;// (codition)?true:false;
+	printf("\n size of a=%d",size(a));
+	(age >= 18)?printf("\neligible "):printf("\nnot eligible");
+	(num>-2)?printf("\npositive"):printf("\n negetive");
+	
+	
+}
