efce80d2040ef74a7d4a98c5d40a9be61b4d9ea5
Manoj Behera <mb647004@gmail.com>
2026-04-22 08:51:27 +0530

    pratical

---
+++ Practical  C/breakjumping.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main(){
+	int i,num;
+	for(i=1;i<=15;i++){
+		if(i==15){
+			continue;
+		}
+		printf("\n i=%d",i);
+	}
+/*	for(;;){
+		printf("\n enter num");
+		scanf("%d",&num);
+		if(num==0){
+			break;
+		}
+	}*/
+}
+
+
+
+
---
+++ Practical  C/example.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+	int a[5]={1,2,3,4,5};
+	int i;
+	printf("\n Enter integer value:");
+	for(i=0;i<5;i++){
+		printf("\n Enter a[%d]=%d",i,a[i]);
+	}
+	
+}
